5ff7cb498dbd7175d81431d0c79331d22a30cd18
dannyardila <dannyardila059@gmail.com>
2022-05-20 09:50:55 -0500

    al fin

---
+++ Bucle_while_calcular_los_numeros_naturales/Bucle-while_suma_de_los_numeros_naturalez.py
@@ -0,0 +1,19 @@
+# Ejercicio while
+
+Suma=0
+j=1
+
+print("Dame el valor de N.\n")
+
+#Input
+n=int(input())
+
+#proccesing 
+while (j <= n):
+    Suma = Suma + j
+    j=j+1
+
+#output
+print("la suma es:",Suma)
+
+#Fin
